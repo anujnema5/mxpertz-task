5eea08d2ff3d5780b7439a86bfedd264dc663aa9
anujnema <anujnema63@gmail.com>
2024-01-12 03:05:17 +0530

    minor change in herosection

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -4,20 +4,22 @@ import { ArrowDownIcon } from '@heroicons/react/24/solid'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import Button from '../ui/Button'
 
 const HeroSection = () => {
   const route = useRouter();
 
   return (
-    <div className='relative w-full h-[30rem] flex items-center justify-center  top-16 bg-food-image bg-cover bg-no-repeat bg-center'>
+    <div className='relative w-full h-[35rem] flex items-center justify-center  top-16 bg-food-image bg-cover bg-no-repeat bg-center'>
         <div className='overlay w-full h-full bg-zinc-900/70 absolute top-0 left-0 flex flex-col items-center justify-center'>
             <div className='flex flex-col gap-10 items-center justify-center w-5/12 text-center text-gray-300'>
                 <h3 className='text-6xl font-semibold'>{headerData.heading}</h3>
                 <p>{headerData.desc}</p>
 
-                <div className='flex gap-5'>
-                    <button className='border-[3px] px-8 uppercase  py-1.5'>View Menu</button>
-                    <button className='bg-[#c39d63]  px-8 uppercase text-white  py-1.5'>Reservation</button>
+                <div className='flex items-center justify-center gap-5'>
+                    <button className='border-[3px] px-8 uppercase  py-1.5 duration-300 ease-in-out transition-all cursor-pointer hover:scale-105 tracking-wider text-sm'>View Menu</button>
+                    <button className='uppercase text-gray-100 bg-[#c39d63] px-10 py-2.5 duration-300 ease-in-out transition-all cursor-pointer hover:scale-105 tracking-wider text-sm'>Reservation</button>
+                    
                 </div>
             </div>
 
